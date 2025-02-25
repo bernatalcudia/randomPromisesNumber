715b547c4dd523497d5c98f97d5bd5886cacf805
bernat alcudia <bernatalcudia@gmail.com>
2025-02-25 10:46:33 +0100

    fixed code 2 resolve promises

--- sprint-06-01-js-asincronia-y-promesas-ejercicio/script.js
+++ sprint-06-01-js-asincronia-y-promesas-ejercicio/script.js
@@ -9,34 +9,37 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-const randomNumber1 = getRandomInt(1, 10);
-const randomNumber2 = getRandomInt(1, 10);
-const randomNumber3 = getRandomInt(1, 10);
-console.log(randomNumber1, randomNumber2, randomNumber3);
 
 const promesa1 = new Promise((resolve, reject) => {
 
+    const randomNumber1 = getRandomInt(1, 10);
     setTimeout(() => {
-        resolve("El numero es :" + randomNumber1);
+        resolve(randomNumber1);
     }, 1000)
 });
 
 const promesa2 = new Promise((resolve, reject) => {
+    const randomNumber2 = getRandomInt(1, 10);
 
     setTimeout(() => {
-        resolve("El numero es :" + randomNumber2);
+        resolve(randomNumber2);
     }, 3000)
 });
 
 const promesa3 = new Promise((resolve, reject) => {
 
+    const randomNumber3 = getRandomInt(1, 10);
     setTimeout(() => {
-        resolve("El numero es :" + randomNumber3);
+        resolve(randomNumber3);
     }, 5000)
 });
 
 
 Promise.all([promesa1, promesa2, promesa3]).then(values => {
-    const randomNumberResult = randomNumber1 + randomNumber2 + randomNumber3;
-    console.log("La suma total es : " + randomNumberResult);
+    let randomNumberResult = 0;
+    values.forEach((value) => {
+        randomNumberResult = randomNumberResult + value
+    })
+
+    console.log(randomNumberResult);
 });
